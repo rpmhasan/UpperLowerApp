d28053df555bcf705f5c8a3f0ffaf5977bea3052
rpmhasan <rpm.hasan@gmail.com>
2015-05-13 16:53:47 +0600

    FULL

---
+++ UpperLowerApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpperLowerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Lower Limit: ");
+            int lower = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter Upper Limit: ");
+            int upper = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nResult is: \n");
+            for (int i = lower; i <=upper ; i++)
+            {
+                Console.WriteLine(i + "\n");
+            }
+            Console.Read();
+        }
+    }
+}
